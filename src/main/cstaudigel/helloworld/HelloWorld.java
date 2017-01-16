c4ed0759ce575716977ef26cb625c7586c3c7ecc
Unknown <staudigelc@xavier.edu>
2017-01-16 17:22:42 -0500

    Hello World + gitignore
    Hello World | Chris Staudigel | Hello world java program

---
+++ src/main/cstaudigel/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package cstaudigel.helloworld;
+
+/**
+ * Created by chris on 1/16/17.
+ *
+ * Cannot run with package "java.xxx.xxx", package cannot start with java.
+ *
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
